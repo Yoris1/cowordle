c5493499c84bb466456ec4d46eb804b3db5d53b4
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-03-02 03:15:09 +0200

    turn grid into proper class

--- express/scripts/grid.js
+++ express/scripts/grid.js
@@ -1,5 +1,62 @@
 // create a grid type, because i hate how javascript looks when i dont have actual types defined
 
+class Grid {
+	set_colors(row, colors) {
+		for(var i = 0; i < colors.length; i++) {
+			this.matrix[row][i].addClass("grid_"+colors[i]);
+		}
+	}
+	reset_row(row) {
+		this.matrix[row].forEach(element => {
+			element.removeClass("grid_good grid_bad grid_mid grid_notreal");
+			element.text('');
+		})
+	}
+	reset() {
+		for(var i = 0; i < this.matrix.length; i++) {
+			this.reset_row(i);
+		}
+	}
+	set_text(text, row) {
+		for(var i = 0; i < text.length; i++)
+			this.matrix[row][i].text(text[i]);
+	}
+	set_name(name) {
+		this.name.text(name);
+	}
+	set_points(points) {
+		this.points.text("points: "+points);
+	}
+	constructor(width, height) {
+		//generate matrix
+		this.container = $(`<div class="grid">`).appendTo($("#gameboard")); // grid div
+		this.name = $(`<p class="boardname">`).appendTo(this.container);
+		this.points = $(`<p class="points">`).appendTo(this.container);
+		this.matrix = []; // all the text elements
+
+		for(var y = 0; y < height; y++) {
+			var row = $(`<div class=grid_row>`).appendTo(this.container);
+			var entries = [];
+			for(var x = 0; x < width; x++) {
+				var entry = $(`<div class=grid_entry>`).appendTo(row);	
+				entries.push(entry);
+			}
+			this.matrix.push(entries);
+		}
+		// =============
+	}
+};
+
+$(document).ready(function() {
+	// delete this function !! this is for testing!! this is bad!!
+	var grid = 	new Grid(5, 6);
+	grid.set_text("test", 0); 
+	grid.set_colors(2, ["bad", "bad", "good", "mid", "bad"]);
+	grid.set_name("test");
+	grid.set_points(100);
+})
+
+
 var grids = {}
 
 function resetGrids() {
