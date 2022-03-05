0a0e040959a75f501e06c0c77d229d6c8075a26a
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-03-05 02:06:34 +0200

    make name class cleaner and store name

--- express/scripts/name_input.js
+++ express/scripts/name_input.js
@@ -1,53 +1,59 @@
 class Name {
-	clear() {
-		$("#name").empty();
-	}
 	type(letter) {
 		letter = letter.toLowerCase();
-		if(!this.allowed_name_letters.includes(letter)) return;
 		if(letter == " ") letter = "_";
-		if(this.clear_string && this.clear_string === true)  {
-			this.clear();
-			this.clear_string = false;
-		}
+		if(!this.allowed_name_letters.includes(letter)) return;
 		if(this.letters.length >= 12) return;
-		console.log(letter);
+
+		this.remove_placeholder();
+
 		this.letters.push(letter);
 		this.elements.push($(`<div class="username_letter">`));
-		console.log(this.elements);
 		this.elements[this.elements.length-1].text(letter).appendTo($("#name"));
+
+		localStorage.setItem("name", this.getName());
 	}
-	test() {
-		this.clear_string = true;
+	placeholder() {
+		console.log(this);
+		if(this.placeholder_active && this.placeholder_active === true) return;
+		this.placeholder_active = true;
 		var str = "username";
 		for(var i = 0; i < str.length; i++) {
 			$(`<div class="username_letter">`).text(str[i]).appendTo($("#name"));
 		}
 	}
-	
+	remove_placeholder() {
+		if(!this.placeholder_active || this.placeholder_active === false) return;
+		this.placeholder_active = false;
+		$("#name").empty();
+	}
 	backspace() {
-		console.log(this.elements);
 		this.elements[this.elements.length-1].remove();
 		this.elements = this.elements.slice(0, -1);
 		this.letters = this.letters.slice(0, -1);
-		if(this.letters.length === 0) {
-			this.test();
-		} 
+		if(this.letters.length === 0) this.placeholder();
+		
+		localStorage.setItem("name", this.getName());
 	}
 	getName() {
-		var str = "";
-		this.letters.forEach(letter => {
-			str += letter;
-		})
+		var str = ""; 
+		this.letters.forEach(letter => {str += letter});
 		return str;
 	}
 	constructor() {
 		this.allowed_name_letters = "qwertyuiopasdfghjklzxcvbnm 1234567890_";
-		setTimeout(() => {
-			this.test();
-		}, 100);
 		this.letters = [];
 		this.elements = [];
+		this.placeholder();
+		
+		var stored_name = localStorage.getItem("name");
+		if(!stored_name) return;
+		for(var i = 0; i < stored_name.length; i++) 
+			this.type(stored_name[i]);
 	}
 }
-var userName = new Name();
+
+var userName;
+$(document).ready(function () {
+	userName = new Name();
+})
