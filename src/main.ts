cf6c74bfe675efc41a51afe74268724345e44b8f
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-03-07 01:52:04 +0200

    obfuscated js

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,32 @@
+import { readdirSync, readFileSync } from "fs";
 import { GameManager } from "./business";
 
 const http = require('http');
 const express = require('express');
 const session = require('express-session');
+const obfuscator = require('javascript-obfuscator');
 
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 var gameManager : GameManager = new GameManager(io);
 
+var obfuscated_js = "";
+function obfuscate_js() {
+	var javascript = "";
+	var filenames = readdirSync("./express/scripts");
+	filenames.forEach((file) => {
+		javascript += readFileSync(`./express/scripts/${file}`, {encoding:'utf8', flag:'r'});
+		javascript += `\n`;
+	});
+	var obfuscated = obfuscator.obfuscate(javascript);
+	obfuscated_js = obfuscated.getObfuscatedCode();
+}
 
 const router = express.Router();
+router.route('/js').get((req, res) => {
+	res.send(obfuscated_js);
+})
 router.route('/room').post((req, res) => {
 	if (req.body) {
 		console.log(`User ${req.sessionID} requested to create a room!`);
@@ -31,6 +47,7 @@ app.use(session({
 app.use(router);
 
 
+obfuscate_js();
 console.log("Starting worlde server 2.0!");
 const port = 8080;
 server.listen(port);
