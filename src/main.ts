93ae24cdf35f22fa269bd4331401a754e0fdde80
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-03-07 03:55:59 +0200

    add option to obfuscate

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+var obfuscate = true;
+
 import { readdirSync, readFileSync } from "fs";
 import path = require("path");
 import { GameManager } from "./business";
@@ -29,7 +31,7 @@ router.route('/post_exec_message_miles').post((req, res) => {
 });
 app.use(function(req, res, next) {
 	const regex = new RegExp('^\/scripts.*\.js$');
-	if(regex.test(req.path)) {
+	if(regex.test(req.path) && obfuscate === true) {
 		console.log("file");
 		var obfuscated = obfuscator.obfuscate(readFileSync(path.join('express', req.path), 'utf-8'), {compact: true}).getObfuscatedCode();
 		res.send(obfuscated);
