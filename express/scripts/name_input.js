44b0bcbcd42ceec40fe83ea0f4db1567e50f938b
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-03-12 20:38:22 +0200

    change secret phrase

--- express/scripts/name_input.js
+++ express/scripts/name_input.js
@@ -1,6 +1,6 @@
 class Name {
 	color_placeholder = false;
-	username_correct = "cat_cafe_sim"
+	username_correct = "cat_coffees"
 	update_letter_colors() {
 		if(!this.letters) return;
 		var res = compare(this.getName(), this.username_correct, false);
