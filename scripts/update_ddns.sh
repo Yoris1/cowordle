f17c86ae614d2db7b71827f1560259e245f79a89
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-03-12 14:01:44 +0200

    make ddns update www and @

--- scripts/update_ddns.sh
+++ scripts/update_ddns.sh
@@ -1,2 +1,3 @@
 source .env
 curl "https://dynamicdns.park-your-domain.com/update?host=$DDNS_HOST&domain=$DOMAIN&password=$DDNS_PASSWORD"
+curl "https://dynamicdns.park-your-domain.com/update?host=$DDNS_HOST2&domain=$DOMAIN&password=$DDNS_PASSWORD"
