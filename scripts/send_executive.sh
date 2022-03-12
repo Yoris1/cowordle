b42be8958321411478aa7187fa906063e3f78f0b
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-03-12 15:04:21 +0200

    make notifications work

--- scripts/send_executive.sh
+++ scripts/send_executive.sh
@@ -1,2 +1,2 @@
 source .env
-curl -d "$1" -d "password=${EXECUTIVE_MESSAGE_PASSWORD}" -X POST https://${DOMAIN}/${EXECUTIVE_MESSAGE_ENDPOINT}
+curl -d "message=$1" -d "password=${EXECUTIVE_MESSAGE_PASSWORD}" -X POST https://${DOMAIN}${EXECUTIVE_MESSAGE_ENDPOINT}
